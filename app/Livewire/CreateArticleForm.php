032d8fed7964474f096fde0d7002964e9df8bee2
Andrea Sdringola <andrea@ufficio>
2025-01-30 00:19:42 +0100

    US 8 Completata

--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Article;
 use Livewire\Component;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
@@ -52,12 +53,19 @@ class CreateArticleForm extends Component
             foreach ($this->images as $image) {
                 $newFileName = "articles/{$this->article->id}";
                 $newImage = $this->article->images()->create(['path' => $image->store($newFileName, 'public')]);
-                dispatch(new ResizeImage($newImage->path, 300, 300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                //vecchio codice:
+                // dispatch(new ResizeImage($newImage->path, 300, 300));
+                // dispatch(new GoogleVisionSafeSearch($newImage->id));
+                // dispatch(new GoogleVisionLabelImage($newImage->id));
+                //nuovo codice:
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path, 300, 300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id)
+                ])->dispatch($newImage->id);
             }
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
-        }        
+        }               
         
         session()->flash('success', 'Articolo creato correttamente');
         $this->cleanForm();
